ca4c282c176fab8e5ee77044708a50af3a53a7d3
jdwcg <jdwcg@nate.com>
2021-11-14 17:56:38 +0900

    2

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,12 @@ $(function(){
     navBtn.mouseenter(function(e){
         e.preventDefault();
         $(this).children('ul > li > ul').stop().slideDown();
-        navBg.stop().css({padding:'20vh'});
+        navBg.stop().css({padding:'200px'});
     })
     navBtn.mouseleave(function(e){
         e.preventDefault();
         navLnb.stop().slideUp('fast');
-        navBg.stop().css({padding:'10px'});
+        navBg.stop().css({padding:'200px'});
     })
     var toggleBtn = $('.toggleBtn');
     toggleBtn.click(function(e){
@@ -19,6 +19,6 @@ $(function(){
         $('.nav').slideToggle();
         $('.toggleBtn').hide();
         $('.closeBtn').show();
-        navBg.stop().css({padding:'50vh'});
+        navBg.stop().css({padding:'200px'});
     })
 })
