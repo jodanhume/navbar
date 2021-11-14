07ced3c9ce9e92549906aa950878476ca8fa4a6b
jdwcg <jdwcg@nate.com>
2021-11-14 17:48:04 +0900

    3

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.nav > ul > li > ul').hide();
+    var navBtn = $('.nav > ul > li');
+    var navLnb = $('.nav > ul > li > ul');
+    var navBg = $('.navBg');
+    navBtn.mouseenter(function(e){
+        e.preventDefault();
+        $(this).children('ul > li > ul').stop().slideDown();
+        navBg.stop().css({padding:'150px'});
+    })
+    navBtn.mouseleave(function(e){
+        e.preventDefault();
+        navLnb.stop().slideUp('fast');
+        navBg.stop().css({padding:'10px'});
+    })
+    var toggleBtn = $('.toggleBtn');
+    toggleBtn.click(function(e){
+        e.preventDefault();
+        $('.nav').slideToggle();
+        $('.toggleBtn').hide();
+        $('.closeBtn').show();
+        navBg.stop().css({padding:'100vh'});
+    })
+})
